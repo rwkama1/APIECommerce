9c59e389895b780bb4a85334746354ccc3349fdd
rwkama1 <rwkamandriw@gmail.com>
2021-02-11 05:00:04 -0300

    workorders

--- routes/order.js
+++ routes/order.js
@@ -45,10 +45,10 @@ router.post('/closeOrder', async function (req, res) {
         res.status(400).send(error.toString());
     }
 });
-router.post('/saveOrder', async function (req, res) {
+router.post('/saveOrder/(:idcard)', async function (req, res) {
     try
     {
-        var dtclient = new Client("72289541",'Client3',"Client3","Client3","","");
+        var dtclient = FactoryLogic.getLUser().getUser(req.params.idcard);
         var savorder=await FactoryLogic.getLOrder().saveOrder(dtclient);     
         res.send(savorder);
     }
@@ -80,5 +80,87 @@ router.post('/cancelOrder', async function (req, res) {
         res.status(400).send(error.toString());
     }
 });
-
+/**********************/
+//GETS */
+router.get('/getOrder/(:id)', async function (req, res) {
+    try
+    {
+        var idnumber=parseInt(req.params.id)
+    var getor=await FactoryLogic.getLOrder().getOrder(idnumber);  
+    res.send(getor);
+     }
+   catch(error)
+   {
+    res.status(400).send(error.toString());
+   }
+});
+router.get('/getPendingOrders', async function (req, res) {
+    try
+    {
+    var getor=await FactoryLogic.getLOrder().getPendingOrders();  
+    res.send(getor);
+     }
+   catch(error)
+   {
+    res.status(400).send(error.toString());
+   }
+});
+router.get('/getClientOrders/(:idcard)', async function (req, res) {
+    try
+    {
+    var getor=await FactoryLogic.getLOrder().getClientOrders(req.params.idcard);  
+    res.send(getor);
+     }
+   catch(error)
+   {
+    res.status(400).send(error.toString());
+   }
+});
+router.get('/getAllOrders', async function (req, res) {
+    try
+    {
+    var getor=await FactoryLogic.getLOrder().getAllOrders();  
+    res.send(getor);
+     }
+   catch(error)
+   {
+    res.status(400).send(error.toString());
+   }
+});
+router.get('/getDeliveredOrders', async function (req, res) {
+    try
+    {
+    var getor=await FactoryLogic.getLOrder().getDeliveredOrders();  
+    res.send(getor);
+     }
+   catch(error)
+   {
+    res.status(400).send(error.toString());
+   }
+});
+//************************** */
+router.post('/deliverOrder/(:id)', async function (req, res) {
+    try
+    {
+    let getorder = await FactoryLogic.getLOrder().getOrder(req.params.id);
+    var deliord=await FactoryLogic.getLOrder().deliverOrder(getorder);  
+    res.send(deliord);
+     }
+   catch(error)
+   {
+    res.status(400).send(error.toString());
+   }
+});
+router.post('/personalOrder/(:id)', async function (req, res) {
+    try
+    {
+    let getorder = await FactoryLogic.getLOrder().getOrder(req.params.id);
+    var persorder=await FactoryLogic.getLOrder().personalOrder(getorder);  
+    res.send(persorder);
+     }
+   catch(error)
+   {
+    res.status(400).send(error.toString());
+   }
+});
 module.exports = router;
